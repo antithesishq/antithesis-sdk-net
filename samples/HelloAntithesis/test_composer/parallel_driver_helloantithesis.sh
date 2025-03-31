4c379d1589bf80b9348b7ca1ecdb3efff22eae12
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2025-03-31 13:49:43 -0400

    Updated parallel_driver_helloantithesis.sh to loop 100 times to reduce logging in Antithesis.

--- samples/HelloAntithesis/test_composer/parallel_driver_helloantithesis.sh
+++ samples/HelloAntithesis/test_composer/parallel_driver_helloantithesis.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-curl --silent --output /dev/null --show-error --fail http://webservice:8080
+for i in {1..100}
+do
+    curl --silent --output /dev/null --show-error --fail http://webservice:8080
+done
