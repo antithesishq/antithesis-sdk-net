b5768d3ee9d517214fa494410b49493f0cd03c8d
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2025-04-23 11:59:21 -0400

    Removed samples/OrleansBank in order to publish it to a dedicated repo later.

--- samples/OrleansBank/Bank.Interfaces/ITellerGrain.cs
+++
@@ -1,7 +0,0 @@
-namespace Bank;
-
-public interface ITellerGrain : IGrainWithIntegerKey
-{
-    [Transaction(TransactionOption.Create)]
-    Task Transfer(IAccountGrain from, IAccountGrain to, int cents);
-}
