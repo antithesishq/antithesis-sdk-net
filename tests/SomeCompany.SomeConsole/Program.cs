d114290944cd0d24450e5813eadaa97819713e93
Dan Terry <dan.terry@antithesis.com>
2026-04-20 16:19:04 -0400

    Fixed EndToEndTests.SomeCompanySomeConsole for the recent GuidanceTracker change. Added Lifecycle.SendEvent calls to simplify reading the test's snapshots.

--- tests/SomeCompany.SomeConsole/Program.cs
+++ tests/SomeCompany.SomeConsole/Program.cs
@@ -2,10 +2,13 @@
 using SomeCompany.SomeConsole;
 
 Lifecycle.SetupComplete();
-
 Assert.Reachable("Program.Main.Reachable");
 
+Lifecycle.SendEvent("AssertionTrackerTests.OnlyWriteFirstPassAndFirstFail");
 AssertionTrackerTests.OnlyWriteFirstPassAndFirstFail();
 
+Lifecycle.SendEvent("GuidanceTrackerTests.BooleanWriteAlways");
 GuidanceTrackerTests.BooleanWriteAlways();
+
+Lifecycle.SendEvent("GuidanceTrackerTests.NumericWriteAsMaxOrMinimizing");
 GuidanceTrackerTests.NumericWriteAsMaxOrMinimizing();
